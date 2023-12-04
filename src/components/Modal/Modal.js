8a9bca8b0890f8bbc7d26a293449062fc23790b4
anjali <anjalidehru@gmail.com>
2023-12-04 18:53:00 +0530

    reuired changes for form

---
+++ src/components/Modal/Modal.js
@@ -0,0 +1,77 @@
+import React from "react";
+import TextField from "@mui/material/TextField";
+import Dialog from "@mui/material/Dialog";
+import DialogActions from "@mui/material/DialogActions";
+import DialogContent from "@mui/material/DialogContent";
+import DialogContentText from "@mui/material/DialogContentText";
+import DialogTitle from "@mui/material/DialogTitle";
+const Modal = (props) => {
+ const { open, onClose } = props;
+ return (
+  <div>
+   <Dialog open={open} onClose={onClose}>
+    <DialogTitle>Edit Data</DialogTitle>
+    <form onSubmit={handleSubmit}>
+     <DialogContent>
+      {/* <DialogContentText>Edit the details below</DialogContentText> */}
+
+      <TextField
+       autoFocus
+       margin="dense"
+       id="name"
+       label="Name"
+       value={selectedRow.name}
+       onChange={(e) => handleChange("name", e.target.value)}
+       type="text"
+       fullWidth
+       required
+       variant="filled"
+      />
+      <TextField
+       autoFocus
+       margin="dense"
+       id="age"
+       label="Age"
+       value={selectedRow.age}
+       onChange={(e) => handleChange("age", e.target.value)}
+       type="number"
+       fullWidth
+       required
+       variant="filled"
+      />
+      <TextField
+       autoFocus
+       margin="dense"
+       id="city"
+       label="City"
+       required
+       value={selectedRow.city}
+       onChange={(e) => handleChange("city", e.target.value)}
+       type="text"
+       fullWidth
+       variant="filled"
+      />
+      <TextField
+       autoFocus
+       margin="dense"
+       id="pinCode"
+       label="Pincode"
+       value={selectedRow.pinCode}
+       onChange={(e) => handleChange("pinCode", e.target.value)}
+       type="number"
+       fullWidth
+       required
+       variant="filled"
+      />
+     </DialogContent>
+     <DialogActions>
+      <Button type="submit"> Submit</Button>
+      <Button onClick={() => setShowModal(false)}>Cancel</Button>
+     </DialogActions>
+    </form>
+   </Dialog>
+  </div>
+ );
+};
+
+export default Modal;
